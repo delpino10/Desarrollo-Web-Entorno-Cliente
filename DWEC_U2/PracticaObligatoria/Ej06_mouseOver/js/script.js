95d34b1f573146827230e9dac2fcf597ec7c110b
delpino10 <jadelpino10@gmail.com>
2024-10-10 22:54:52 +0200

    practica 2 hasta ejercicio 7

--- DWEC_U2/PracticaObligatoria/Ej06_mouseOver/js/script.js
+++ DWEC_U2/PracticaObligatoria/Ej06_mouseOver/js/script.js
@@ -1,9 +1,21 @@
+// Accedemos al nodo
 var foto = document.getElementById('foto');
-
+// Accedemos al nodo del input del formulario
+const nombre = document.getElementById('nombre');
+var n = nombre.value;
+// Si el usuario pasa el ratón encima de la imagen
 foto.addEventListener('mouseover', () =>{
     foto.src='./img/2.jpg';
 })
-
+// Si el usuario saca el ratón fuera de la imagen
 foto.addEventListener('mouseout', () =>{
     foto.src='./img/1.jpg';
 })
+// Cuando el usuario no ha ingresado ningún dato, 
+// salta una alerta
+function b(){
+    
+        alert('No ha ingresa ningún dato');
+    
+}
+
