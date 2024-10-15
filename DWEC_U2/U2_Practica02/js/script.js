ae4ed2339becbeaddcb3700c05e7d4e71e0a1656
delpino10 <jadelpino10@gmail.com>
2024-10-15 08:09:08 +0200

    co

---
+++ DWEC_U2/U2_Practica02/js/script.js
@@ -0,0 +1,15 @@
+// const newELe = document.createElement("p");
+// function rombo(){
+//     let n = 5; // altura del rombo
+//     for (let i = 0; i < n; i++) {
+//         var espacios = ' '.repeat(n + 1);
+//         var asteriscos = '*'.repeat(n);     
+//         console.log(espacios + asteriscos);
+//     }
+// }
+
+function hola(){
+    let h = "hola";
+   return h;
+}
+console.log(hola());
