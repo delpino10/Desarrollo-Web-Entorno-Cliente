a634b193d391686cab754735789b8c50c1f3f6e7
delpino10 <jadelpino10@gmail.com>
2024-11-04 10:52:01 +0100

    Ejercicio 01 Comentado

--- DWEC_U2/U3_Practica/js/script.js
+++ DWEC_U2/U3_Practica/js/script.js
@@ -1,5 +1,6 @@
 let y = document.getElementById("n");
 let id = document.getElementById("id");
+let h = document.getElementById("hora");
 
 // Número aleatorio redondeado al primer decimal
 let num = Math.round(Math.random() * 10) / 10;
@@ -8,23 +9,40 @@ let nodo = document.createTextNode(num);
 // los añadimos al nodo asociado al id
 y.appendChild(nodo);
 
+// Creamos el objeto Date 
+let date = new Date;
+// Obtenemos la hora actual
+let horas = date.getHours();
+// Creamos un nodo de texto con la hora
+let nodoHora = document.createTextNode(horas);
+// Lo anidamos a su correspondiente nodo
+h.appendChild(nodoHora);
+
 function funcion(num){
+    // Si el número random es superior o igual a 0.5
     if (num >= 0.5){
+        // Creamos un elemento <a>
         let link = document.createElement("a");
+        // Le aññadimos la URL
         link.href = "https://www.marca.com";
+        // Le adjuntamos el texto del enlace
         link.textContent = "marca.com"
+        // Lo anidamos a su correspondiente nodo
         id.appendChild(link); 
     }else{
-        let date = new Date;
-        let horas = date.getHours;
-
+        // Si son más de la 3 de la tarde
         if(horas >= 15){
+            // Insertamos este texto
             let bt = "Buenas Tardes";
             id.innerHTML = bt;
         }else{
+            // Si no, este otro texto
             let bd = "Buenos Dias"
             id.innerHTML = bd;
         }
     }
 }
+
+// Llamamos a la función
+funcion(num);
        
