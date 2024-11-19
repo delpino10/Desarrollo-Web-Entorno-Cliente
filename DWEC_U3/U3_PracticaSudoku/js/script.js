160545a3229bac4d09bc14c360ee74825f009555
Jose Antonio del Pino <jadelpino10@gmail.com>
2024-11-19 09:25:26 +0200

    Practica sudoku iniciada

--- DWEC_U3/U3_PracticaSudoku/js/script.js
+++ DWEC_U3/U3_PracticaSudoku/js/script.js
@@ -1,1 +1,25 @@
-alert('hola');
+
+function crearTablero(){
+    const tablero = [];
+    for (let i = 0; i < 9; i++) {
+        tablero[i] = []
+        for (let j = 0; j < 9; j++) {
+            tablero[i][j] = 0
+        }
+    }
+    return tablero;
+}
+
+function mostrarTablero(tablero){
+const sudoku = document.getElementById('sudoku');
+    for (let i = 0; i < 9; i++) {
+        const fila = document.createElement('tr')
+        for (let j = 0; j < 9; j++) {
+            const celda = document.createElement('td')
+        }
+    }
+}
+
+
+
+
