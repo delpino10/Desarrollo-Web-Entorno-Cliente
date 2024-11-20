09d2c00c8d40ccd496a3f3891b2fb4ea7a729e0a
Jose Antonio del Pino <jadelpino10@gmail.com>
2024-11-20 11:26:32 +0200

    seguimos con el sudoku

--- DWEC_U3/U3_PracticaSudoku/js/script.js
+++ DWEC_U3/U3_PracticaSudoku/js/script.js
@@ -1,23 +1,27 @@
 
-function crearTablero(){
-    const tablero = [];
-    for (let i = 0; i < 9; i++) {
-        tablero[i] = []
-        for (let j = 0; j < 9; j++) {
-            tablero[i][j] = 0
-        }
-    }
-    return tablero;
-}
+// function crearTablero(){
+//     const tablero = [];
+//     for (let i = 0; i < 9; i++) {
+//         tablero[i] = []
+//         for (let j = 0; j < 9; j++) {
+//             tablero[i][j] = 0
+//         }
+//     }
+//     return tablero;
+// }
 
-function mostrarTablero(tablero){
-const sudoku = document.getElementById('sudoku');
+function mostrarTablero(){
+const contenedor = document.getElementById('contenedor');
+const tabla = document.createElement('table');
     for (let i = 0; i < 9; i++) {
         const fila = document.createElement('tr')
         for (let j = 0; j < 9; j++) {
             const celda = document.createElement('td')
+            fila.appendChild(celda);
         }
+        tabla.appendChild(fila)
     }
+    contenedor.appendChild(tabla);
 }
 
 
