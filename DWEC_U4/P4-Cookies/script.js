93b169e610b8de95440503ef169a73a08f3ae816
Jose Antonio del Pino <jadelpino10@gmail.com>
2024-12-16 11:58:53 +0200

    Practica cookies terminada

--- DWEC_U4/P4-Cookies/script.js
+++ DWEC_U4/P4-Cookies/script.js
@@ -1,57 +1,48 @@
-let imprSesion = document.getElementById('contenedor-cookie');
+// Guardamos el nodo donde se van insertar el número de visitas
+let impSesion = document.getElementById('contenedor-cookie');
 
-// Crear Cookie
-document.cookie= "usuario = Jose Antonio; expires=15 dec 2025 00:00:00 UTC; path=/"
-/*
+// Crear variable y asignar valor desde cookie
+let visitas = document.cookie.replace(/(?:(?:^|.*;\s*)visitas\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+// Asegurar que tiene un valor entero o asignar cero si algo falla
+visitas = parseInt(visitas) || 0;
 
-// Contar sesiones
-function contarSesiones(){
-    if (!localStorage.getItem('sesiones')) {
-        localStorage.setItem('sesiones', 1);
-    } else {
-        let sesiones = parseInt(localStorage.getItem('sesiones'))
-        sesiones++;
-        localStorage.setItem('sesiones', sesiones)
-    }
-
-    return parseInt(localStorage.getItem('sesiones'))
+// si el usuario acepta las cookies
+function aceptaCookies(){
+// Incrementar
+visitas ++;
+// Guardar cookie que expire en 365 días
+document.cookie = 'visitas=' + visitas + ';';
+// Nombre de Usuario
+document.cookie = 'Usuario=JoseAntonio'
+// Expira en un 1 año = 31104000 segundos
+document.cookie = 'max-age = 31104000'
+// Imprimimos el número de visitas en la web
+impSesion.innerHTML = 'Total visitas=' + visitas + ';'
+}
+// Si no acepta las cookies, las borramos
+function borrarCookies(){
+    // dejamos vacio el valor de usuario
+    document.cookie = 'Usuario=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    // Dejamos el contador de visitas a 0
+    visitas = 0;
+    // Actulizamos la cookie con el nuevo valor de 'visitas'
+    document.cookie = 'visitas=' + visitas + ';';
+    // Imprimimos el número de visitas
+    impSesion.innerHTML = 'Total visitas=' + visitas
+    // Lanzamos un alert box para confirmar que las cookies han sido borradas
+    alert('Cookies Borradas')
 }
 
-
-*/
-
-let sumaSesiones = 0;
-// Función para sumar el número de sesiones almacenadas en cookies
-function sumarSesionesDesdeCookies() {
-    let cookies = document.cookie.split('; ');  // Separar las cookies por punto y coma
-
-    cookies.forEach(cookie => {
-        let par = cookie.split('=');
-        let nombre = par[0];
-        let valor = parseInt(par[1]);
-
-        // Verificar si la cookie contiene "sesiones"
-        if (nombre === 'sesiones') {
-            sumaSesiones += valor;
-        }
-    });
-
-    return sumaSesiones;
-}
-
-// Llamar la función para sumar sesiones
-let totalSesiones = sumarSesionesDesdeCookies();
-console.log("Total de sesiones:", totalSesiones);
-
-function pedirPermiso(){
-    let mensaje = "Este sitio web almacena cookie para mejorar la experiencia de usuario\n" +
-        "¿Desea continuar?"
-    if (confirm(mensaje)){
-        sumarSesionesDesdeCookies();
+// Preguntamos al usuario si quiere guardar las cookies
+function ventanaMensaje(){
+let mensaje = "Quiere guardar las cookies de su sesión";
+    // Si acepta, guardamos las cookies
+    if(confirm(mensaje)){
+        aceptaCookies()
     }else{
-
+        // Si no acepta, las borramos
+        borrarCookies()
     }
 }
-
-pedirPermiso()
-
+// Aparece una ventana para preguntar al usuario si quiere guardar las cookies
+ventanaMensaje();
