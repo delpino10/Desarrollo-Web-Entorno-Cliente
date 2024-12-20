d8fd37157cbff89f40ad133760cc5ba78bff9cd0
Jose Antonio del Pino <jadelpino10@gmail.com>
2024-12-20 12:47:39 +0100

    Unidad 4, Práctica 2, hasta ejercicio 2 hecho

--- DWEC_U4/P2-Ejercicios/js/script.js
+++ DWEC_U4/P2-Ejercicios/js/script.js
@@ -6,13 +6,55 @@ function cambiaParrafo(){
 
     btn.addEventListener('click', function () {
         id.style.fontWeight = 'bold'
-        id.style.fontSize='30px';
+        id.style.fontSize='20px';
+        id.style.border='2px solid rgb(0, 183, 235)';
     })
 }
 
 // Ejercicio 2
 // Validaciones
-let nombre = document.getElementById('nombre')
-let apellido = document.getElementById('apellido')
+let btn_submit = document.getElementById('btn-submit');
+
+function validarCampo(campo){
+    const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s'-]+$/;
+    if (campo.length === 0){
+        return "El campo nombre no puede estar vacío"
+    }else if (!regex.test(campo)){
+        return "Caracteres no admitidos"
+    }else{
+        return "Nombre válido"
+    }
+}
+
+
+btn_submit.addEventListener('click', function (){
+
+    let nombre = document.getElementById('nombre').value;
+    const nombreValido = validarCampo(nombre);
+    let input1 = document.getElementById('nombre')
+    const errorNombre = document.getElementById('errorNombre');
+        if("Nombre válido" !== nombreValido){
+            errorNombre.textContent = validarCampo(nombre);
+            errorNombre.style.color = 'red';
+            input1.style.border = '1px solid red'
+        }else {
+            errorNombre.textContent = "";
+            input1.style.border = '1px solid black'
+        }
+
+    let apellido = document.getElementById('apellido').value;
+    const apellidoValido = validarCampo(apellido);
+    let input2 = document.getElementById('apellido')
+    const errorApellido = document.getElementById('errorApellido');
+        if ("Nombre válido" !== apellidoValido){
+            errorApellido.textContent = validarCampo(apellido);
+            errorApellido.style.color = 'red';
+            input2.style.border = '1px solid red'
+        }else {
+            errorApellido.textContent="";
+            input2.style.border = '1px solid black'
+    }
+});
+
 
 
