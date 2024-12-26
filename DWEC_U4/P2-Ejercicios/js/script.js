b79589f39937e509ee11fd439f1864ce5f75a992
Jose Antonio del Pino <jadelpino10@gmail.com>
2024-12-26 11:38:07 +0100

    Practica 2 de la Unidad 4 => terminada

--- DWEC_U4/P2-Ejercicios/js/script.js
+++ DWEC_U4/P2-Ejercicios/js/script.js
@@ -1,9 +1,14 @@
 // Ejercicio 1
+// Selecciona un párrafo y le cambia el estilo para hacerlo más grande
+// Se ejecuta desde HTML
 function cambiaParrafo(){
+    // Accedemos al valor del input text donde nos dice que párrafo selecciona
     let num = document.getElementById('numParrafo').value;
+    // Le decimos que párrafo es el particular
     let id = document.getElementById("p" + num);
+    // El botón para ejecutar la elección
     let btn = document.getElementById('btn')
-
+    // Evento 'click' para cambiar los estilos
     btn.addEventListener('click', function () {
         id.style.fontWeight = 'bold'
         id.style.fontSize='20px';
@@ -14,19 +19,24 @@ function cambiaParrafo(){
 
 // Ejercicio 2
 // Validaciones
+// Botón para enviar el formulario
 let btn_submit = document.getElementById('btn-submit');
-
+// Validaciones de un campo de texto
 function validarCampo(campo){
+    // Expresión regular
     const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s'-]+$/;
+    // El campo no puede tener cero caracteres
     if (campo.length === 0){
         return "El campo nombre no puede estar vacío"
+    // El campo no puede incluir caracteres especificados fuera del regex
     }else if (!regex.test(campo)){
         return "Caracteres no admitidos"
     }else{
+    // Cumple todo los anterior
         return "Nombre válido"
     }
 
-
+// Valida un número de teléfono
 }function validarTelefono(campo){
     const regex = /^[67]\d{8}$/
     if (campo.length === 0){
@@ -38,24 +48,32 @@ function validarCampo(campo){
     }
 }
 
-
+// Envía los datos al servidor  tras hacer 'click'
 btn_submit.addEventListener('click', function (event){
-
+    // PReviene de enviar los datos al servidor sin validar
     event.preventDefault();
+    // Acedemos al valor del campo nombre
     let nombre = document.getElementById('nombre').value;
+    // Validamos ese valor
     const nombreValido = validarCampo(nombre);
     let input1 = document.getElementById('nombre')
+    // Accedemos al nodo donde se muestra el error
     const errorNombre = document.getElementById('errorNombre');
+        // si el nombre no está validado se muestra el error
         if("Nombre válido" !== nombreValido){
+            // No se envía al servidor
             event.preventDefault()
+            // Se muestra el tipo de error
             errorNombre.textContent = validarCampo(nombre);
+            // Se decora el error
             errorNombre.style.color = 'red';
             input1.style.border = '2px solid red'
         }else {
+            // si no hay error, se deja vacío.
             errorNombre.textContent = "";
             input1.style.border = '2px solid rgba(255, 165, 0,.3)'
         }
-
+// Se hace lo mismo con el campo apellido
     let apellido = document.getElementById('apellido').value;
     const apellidoValido = validarCampo(apellido);
     let input2 = document.getElementById('apellido')
@@ -69,6 +87,7 @@ btn_submit.addEventListener('click', function (event){
             errorApellido.textContent="";
             input2.style.border = '2px solid rgba(255, 165, 0,.3)'
     }
+    // se hace los mismo con el campo teléfono
     let telefonoValue = document.getElementById('telefono').value;
     const telefValido = validarTelefono(telefonoValue);
     let telef = document.getElementById('telefono')
@@ -86,16 +105,20 @@ btn_submit.addEventListener('click', function (event){
 });
 
 // Ejercicio 3 (Email) y 4 (Teléfono)
+// Utilizamos la validaciones incluidas en el navegador
+// Accedemos al formulario completo
 const formulario = document.querySelector('form');
-
+// Cuando vayamos a enviar el formulario al servidor
 formulario.addEventListener('submit', function (event){
     let email = document.getElementById('email');
     let errorEmail = document.getElementById('errorEmail');
-
+    // Comprobamos que el email tiene un formato correcto
     if(!email.checkValidity()){
         event.preventDefault();
+        // Mostramos el error si el email no es válido
         errorEmail.textContent = email.validationMessage;
     }else{
+        // si es válido no mostramos nada
         errorEmail.textContent = "";
     }
 });
@@ -104,9 +127,12 @@ formulario.addEventListener('submit', function (event){
 
 // Ejercicio 8
 
+// Accedemos al nodo del párrafo
 let parrafo = document.getElementById('p')
 
+// Asociamos el evento al párrafo
 parrafo.addEventListener('mouseover', () => {
+    // Introducimos en la web el texto del párrafo, pero transformado en mayúsculas
     parrafo.innerHTML = parrafo.textContent.toUpperCase();
 });
 
@@ -142,9 +168,9 @@ btn_open.addEventListener('click', () => {
 btn_resizeTo.addEventListener('click',  () => {
     let alto = document.getElementById('alto').value
     let ancho = document.getElementById('ancho').value
-    // Redimensinar las página
+    // Redimensinar las páginas
     ventana.resizeTo(ancho, alto)
-    // Mostramos la nuevas medidas de la página
+    // Mostramos las nuevas medidas de la página
     ventana.document.write(`Altura: ${ventana.outerHeight}px | Ancho: ${ventana.outerWidth}px`);
 
 })
@@ -160,6 +186,72 @@ btn_resizeBy.addEventListener('click',  () => {
 btn_cerrar.addEventListener('click', () =>{
     ventana.close();
 })
+
+
+
+
+// Ejercicio 10
+//Accedemos al botón para recargar la página
+let reload_btn = document.getElementById('reset-button');
+// Accedemos al botón para activar el evento
+let btn_ej10 = document.getElementById('btn-ej10');
+// Accedemos al botón para borrar el evento
+let btn_remove = document.getElementById('btn-ej10-remove')
+
+// El evento para cuando se hace click
+const eventoClick = () => {
+    alert('Has hecho click!!')
+}
+// Evento para cuando se pasa ratón por encima del botón
+const eventoMouseOver = () => {
+    btn_ej10.textContent = "Has pasado sobre mi"
+}
+// Evento para cuando el ratón sale del botón
+const eventoMouseOut = () => {
+    btn_ej10.textContent = "Has salido de mi"
+}
+
+// Recargamos la página tras pulsar el botón
+reload_btn.addEventListener('click', () => {
+    location.reload()
+})
+
+// Definimos los eventos al mismo botóm
+btn_ej10.addEventListener('click', eventoClick)
+btn_ej10.addEventListener('mouseover', eventoMouseOver)
+btn_ej10.addEventListener('mouseout', eventoMouseOut)
+
+// Creamos un array con los nombres de los eventos que queremos eliminar
+let eventosEliminar = ['click', 'mouseover', 'mouseout']
+
+// Cada vez que pulsemos el botón  eliminaremos un evento
+btn_remove.addEventListener('click', () => {
+    // Mientras el array tenga algun valor dentro
+    if(eventosEliminar.length > 0 ){
+        // Guardamos la primera posición del array en una const
+        const evento = eventosEliminar.shift()
+            // Recorremos el array y vamos eliminando los eventos
+            if(evento === 'click'){
+                // Evento para hacer click. (Debe estar escrito tal cual se creó)
+                btn_ej10.removeEventListener('click', eventoClick)
+            }else if(evento === 'mouseover'){
+                // Evento para hacer para pasar el ratón por encima. (Debe estar escrito tal cual se creó)
+                btn_ej10.removeEventListener('mouseover', eventoMouseOver)
+            }else if (evento === 'mouseout'){
+                // Evento para cuando el ratón sale del botón. (Debe estar escrito tal cual se creó)
+                btn_ej10.removeEventListener('mouseout', eventoMouseOut)
+            }
+            // Ventana pop-up que muestra que evento se ha eliminado
+            alert(`${evento} ha sido eliminado`)
+
+
+    }else {
+        // Ventana que meustra que cuando todos los eventos han sido eliminados
+        alert("Todos los eventos han sido eliminados")
+    }
+})
+
+
 
 
 
--- DWEC_U4/P2-Ejercicios/js/scriptQuestionario.js
+++ DWEC_U4/P2-Ejercicios/js/scriptQuestionario.js
@@ -1,15 +1,18 @@
+// Accedemos al nodo del formulario
 let formulario = document.getElementById('formulario');
-
-
+// Ejecutamos un evento submit pra enviar los datos del formulario
 formulario.addEventListener('submit', function (event) {
+// Contadores para evaluar el número de respuestas y proporcionar un veredicto
 let contadorLeve = 0;
 let contadorGrave = 0;
 let contadorMedio = 0;
+// Nodo para mostrar el resultado
 let resultado = document.getElementById('resultado');
-let emoji = document.querySelector('.grave')
-
+    // Si nos válido no se envían
     event.preventDefault();
+    // Accedemos a cada pregunta a través del 'name'
     let p1Seleccionada = document.querySelector('input[name = "pregunta1"]:checked');
+    // Usamos un ternario. Si la pregunta tiene un valor elegido, pasamos el valor, si no pasamos 'null'
     let p1 = p1Seleccionada ? p1Seleccionada.value : null;
     let p5Seleccionada = document.querySelector('input[name = "pregunta5"]:checked');
     let p5 = p5Seleccionada ? p5Seleccionada.value : null;
@@ -18,18 +21,22 @@ let emoji = document.querySelector('.grave')
     let p7Seleccionada = document.querySelector('input[name = "pregunta7"]:checked');
     let p7 = p7Seleccionada ? p7Seleccionada.value : null;
 
+    // Creamos un array de los valores seleccionados en los checkboxes por el usuario
     const p2 = Array.from(document.querySelectorAll('input[name="pregunta2"]:checked'))
+        // Itera sobre cada elemento seleccionado y obtiene su valor (value).
         .map(checkbox => checkbox.value);
     const p3 = Array.from(document.querySelectorAll('input[name="pregunta3"]:checked'))
         .map(checkbox => checkbox.value);
     const p4 = Array.from(document.querySelectorAll('input[name="pregunta4"]:checked'))
         .map(checkbox => checkbox.value);
 
-
+    // Alojamos los valores obtenidos de los inputs radios previamente en un array
     const valoresRadio = [p1, p5, p6, p7];
-
+    // Recorremos ese array
     valoresRadio.forEach(valor=>{
+        // Si la pregunta tiene un 'value' de 0 o 1 y no es null
         if(valor <= 1 && valor !== null){
+        // el contador aumenta en uno
             contadorLeve++;
         }else if(valor > 1 && valor < 3 && valor !== null){
             contadorMedio++;
@@ -38,8 +45,11 @@ let emoji = document.querySelector('.grave')
         }
     });
 
+    // Alojamos los valores obtenidos de los inputs radios previamente en un array
     const valoresCheckBox = [p2, p3, p4];
-
+    // Recorremos ese array.
+    // Dependiendo de la cantidad de opciones seleccionadas,
+    // aumentaremos un contador u otro
     valoresCheckBox.forEach(valor =>{
         if(valor.length === 1){
             contadorLeve++;
@@ -50,19 +60,23 @@ let emoji = document.querySelector('.grave')
         }
     })
 
+    // Resultados
+    // Si el contador Grave es mayor que el resto, mostrará un mensaje
     if((contadorGrave > contadorMedio) && (contadorGrave > contadorLeve)){
-        resultado.innerHTML = "Riesgo Grave: Estás siendo víctima de acoso. Por favor, comunícaselo a tus profesores"
+        resultado.innerHTML = "&#x1F534; Riesgo Grave: Estás siendo víctima de acoso. Por favor, comunícaselo a tus profesores"
         resultado.style.fontWeight='700'
+    // Si el contador Medio es mayor que el resto, mostrará un mensaje
     }else if ((contadorMedio > contadorGrave) && (contadorMedio > contadorLeve)){
-        resultado.innerHTML = "Riesgo medio: Ten cuidado, puede ir a más "
+        resultado.innerHTML = "&#x1F7E0; Riesgo medio: Ten cuidado, puede ir a más "
         resultado.style.fontWeight='700'
+    // Si el contador Leve es mayor que el resto, mostrará un mensaje
     }else if((contadorLeve > contadorGrave) && (contadorLeve > contadorMedio)){
-        resultado.innerHTML = "Riesgo Leve: No tienes ningún indicio de ser víctima de acoso"
+        resultado.innerHTML = "&#x2705; Riesgo Leve: No tienes ningún indicio de ser víctima de acoso"
         resultado.style.fontWeight='700'
     }
 
 
-    console.log('pregunta 1:', p1)
+    /*console.log('pregunta 1:', p1)
     console.log('pregunta 2:', p2)
     console.log('pregunta 3:', p3)
     console.log('pregunta 4:', p4)
@@ -71,7 +85,7 @@ let emoji = document.querySelector('.grave')
     console.log('pregunta 7:', p7)
     console.log('cont Leve:', contadorLeve)
     console.log('cont Medio:', contadorMedio)
-    console.log('cont Grave:', contadorGrave)
+    console.log('cont Grave:', contadorGrave)*/
 
 
 
