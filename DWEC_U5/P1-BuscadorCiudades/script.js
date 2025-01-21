048bb32758b753fd4669571f7765a8bf7aabb40d
Jose Antonio del Pino <jadelpino10@gmail.com>
2025-01-21 09:09:41 +0200

    seguimos practica busqueda ciudades con AJAX

---
+++ DWEC_U5/P1-BuscadorCiudades/script.js
@@ -0,0 +1,18 @@
+// Usar XmlHttpRequest y Fetch
+const valorInput = document.getElementById('ciudadInput')
+
+valorInput.addEventListener('input',
+    () => {
+        const ciudadBuscada = valorInput.value.trim().toLowerCase();
+
+        if (ciudadBuscada.length === 0) {
+            console.log('input vacío')
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'ciudades.json', true);
+        xhr.onreadystatechange = function () {
+
+        }
+
+    })
